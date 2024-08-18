59500218298ee059d81cec6b88f445fe7c4339fc
hitendra1908 <hitendra1908@gmail.com>
2024-08-18 23:49:45 +0200

    run.sh file fixed command to run on all machines

--- run.sh
+++ run.sh
@@ -11,30 +11,30 @@ function build_api() {
 #Starting the dependencies needed for the API for eg, DB
 function start_infra() {
     echo "Starting infra docker containers...."
-    docker-compose -f ${dc_infra} up -d
-    docker-compose -f ${dc_infra} logs -f
+    docker compose -f ${dc_infra} up -d
+    docker compose -f ${dc_infra} logs -f
 }
 
 #Stopping the dependencies of the API for eg, DB
 function stop_infra() {
     echo "Stopping infra docker containers...."
-    docker-compose -f ${dc_infra} stop
-    docker-compose -f ${dc_infra} rm -f
+    docker compose -f ${dc_infra} stop
+    docker compose -f ${dc_infra} rm -f
 }
 
 #Staring/Running the API with its dependencies
 function start() {
     build_api
     echo "Starting all docker containers...."
-    docker-compose -f ${dc_infra} -f ${dc_app} up --build -d
-    docker-compose -f ${dc_infra} -f ${dc_app} logs -f
+    docker compose -f ${dc_infra} -f ${dc_app} up --build -d
+    docker compose -f ${dc_infra} -f ${dc_app} logs -f
 }
 
 #Stopping the API with its dependencies
 function stop() {
     echo "Stopping all docker containers...."
-    docker-compose -f ${dc_infra} -f ${dc_app} stop
-    docker-compose -f ${dc_infra} -f ${dc_app} rm -f
+    docker compose -f ${dc_infra} -f ${dc_app} stop
+    docker compose -f ${dc_infra} -f ${dc_app} rm -f
 }
 
 #Function to restart the API
